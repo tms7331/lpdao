254561430c89c82af5f253a23793e9e39e24c3e2
Thomas <thomasr928@gmail.com>
2024-09-16 12:01:01 -0500

    Finalize auction extension with tests

---
+++ src/AuctionExtension.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {Currency, CurrencyLibrary} from "v4-core/src/types/Currency.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
+
+
+contract Auction {
+    using PoolIdLibrary for PoolKey;
+
+    uint256 public constant MINIMUM_BID = 0.01 ether;
+    address public hook;
+    address public poolManager;
+    // PoolKey => block number => winning bid
+    mapping(PoolId => mapping(uint256 => Bid)) public winningBids;
+
+    struct Bid {
+        address bidder;
+        uint256 amount;
+    }
+
+    constructor(address _hook, address _poolManager) {
+        hook = _hook;
+        poolManager = _poolManager;
+    }
+
+    // Function to place a bid in a specific pool for a specific block number
+    function placeBid(PoolKey calldata key, uint256 blockNumber) external payable {
+        require(msg.value >= MINIMUM_BID, "Bid too low!");
+
+        Bid memory currentWinningBid = winningBids[key.toId()][blockNumber];
+        require(msg.value > currentWinningBid.amount, "Bid must be higher than current winning bid");
+
+
+        // If we have a previous bid, need to refund that user's ETH
+        // Security issue - if winning bidder is a contract without a receive function this could fail?
+        if (currentWinningBid.amount > 0) {
+            (bool success, ) = currentWinningBid.bidder.call{value: currentWinningBid.amount}("");
+            require(success, "ETH transfer failed");
+        }
+
+        winningBids[key.toId()][blockNumber] = Bid({
+            bidder: msg.sender,
+            amount: msg.value
+        });
+    }
+
+    function checkAddLiquidity(address, PoolKey calldata, IPoolManager.SwapParams calldata, bytes calldata) public view returns (bool) {
+        return true;
+    }
+
+    function checkSwap(address swapper, PoolKey calldata key, IPoolManager.SwapParams calldata, bytes calldata) public returns (bool) {
+        require(msg.sender == hook, "Only hook!");
+        PoolId poolId = key.toId();
+        Bid memory currentWinningBid = winningBids[poolId][block.number];
+        uint bidEthAmount = currentWinningBid.amount;
+        // If bid is 0 - there was no bid or we already processed it
+        if (bidEthAmount == 0) {
+            return true;
+        }
+        // Otherwise we need to check that the swap is from the auction winner
+        require(swapper == currentWinningBid.bidder, "Only auction winner can call this function");
+
+        // If we've made it here - it's the winning bidder, let the swap go through and donate funds to pool
+        delete winningBids[poolId][block.number];
+
+        // Also want to donate the ETH to the pool
+        if (Currency.unwrap(key.currency0) == address(0)) {
+            IPoolManager(poolManager).donate(key, bidEthAmount, 0, "");
+        } else {
+            // We check that one of the currencies will be ETH in the hook
+            IPoolManager(poolManager).donate(key, 0, bidEthAmount, "");
+        }
+
+        return true;
+    }
+
+    function donateBid(PoolKey calldata key, uint256 blockNumber) external {
+        // If they win a bid but don't swap, their bid amount is locked in the contract
+        // So let anyone call this to donate past bids...
+        PoolId poolId = key.toId();
+        Bid memory currentWinningBid = winningBids[poolId][blockNumber];
+        require(blockNumber < block.number, "Must be a past block");
+        require(currentWinningBid.amount > 0, "No bid to donate");
+        uint bidEthAmount = currentWinningBid.amount;
+        delete winningBids[poolId][block.number];
+
+        if (Currency.unwrap(key.currency0) == address(0)) {
+            IPoolManager(poolManager).donate(key, bidEthAmount, 0, "");
+        } else {
+            // We check that one of the currencies will be ETH in the hook
+            IPoolManager(poolManager).donate(key, 0, bidEthAmount, "");
+        }
+    }
+}
+
---
+++ test/AuctionExtension.t.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import {Auction} from "../src/AuctionExtension.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {Currency, CurrencyLibrary} from "v4-core/src/types/Currency.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
+import {BaseHook} from "v4-periphery/src/base/hooks/BaseHook.sol";
+import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+
+contract FakePoolManager {
+    function donate(PoolKey memory key, uint256 amount0, uint256 amount1, bytes calldata hookData)
+        external
+        returns (BalanceDelta)
+    {
+        BalanceDelta zeroDelta = BalanceDelta.wrap(0);
+        return zeroDelta;
+    }
+}
+
+
+contract AuctionTest is Test {
+    Auction auction;
+    address hook = address(0x456);
+    address poolManager;
+    PoolKey key;
+    
+    address user1 = address(0x111);
+    address user2 = address(0x222);
+    uint256 blockNumber = 100;
+    uint256 minBid = 0.01 ether;
+
+    function setUp() public {
+        // Set up initial values for the PoolKey (mocked)
+
+        FakePoolManager fakePoolManager = new FakePoolManager();
+        poolManager = address(fakePoolManager);
+
+        key = PoolKey(Currency.wrap(address(0)), Currency.wrap(address(0xABC)), 3000, 60, IHooks(hook));
+
+        // Deploy Auction contract
+        auction = new Auction(hook, poolManager);
+
+        // Label addresses for easier debugging
+        vm.label(hook, "Hook");
+        vm.label(poolManager, "Pool Manager");
+        vm.label(user1, "User 1");
+        vm.label(user2, "User 2");
+    }
+
+    function testCannotBidBelowMinimum() public {
+        vm.deal(user1, 1 ether);
+        vm.prank(user1);
+        vm.expectRevert("Bid too low!");
+        auction.placeBid{value: 0.005 ether}(key, blockNumber);
+    }
+
+    function testPlaceBid() public {
+        vm.deal(user1, 1 ether);
+        vm.prank(user1);
+        auction.placeBid{value: minBid}(key, blockNumber);
+
+        (address bidder, uint256 amount) = auction.winningBids(key.toId(), blockNumber);
+        assertEq(bidder, user1);
+        assertEq(amount, minBid);
+    }
+
+
+    function testOutbidAndRefundPreviousBidder() public {
+        // First bid by user1
+        vm.deal(user1, 1 ether);
+        vm.prank(user1);
+        auction.placeBid{value: minBid}(key, blockNumber);
+
+        // Second bid by user2 with higher value
+        vm.deal(user2, 2 ether);
+        vm.prank(user2);
+
+        uint256 user1BalanceBefore = user1.balance;
+        auction.placeBid{value: 0.02 ether}(key, blockNumber);
+        
+        (address bidder, uint256 amount) = auction.winningBids(key.toId(), blockNumber);
+        assertEq(bidder, user2);
+        assertEq(amount, 0.02 ether);
+
+        uint256 user1BalanceAfter = user1.balance;
+        // Make sure we refunded the first user
+        assertEq(user1BalanceAfter, user1BalanceBefore + minBid);
+    }
+
+
+    function testSwapByWinningBidder() public {
+        // Place a bid first
+        vm.deal(user1, 1 ether);
+        vm.prank(user1);
+        auction.placeBid{value: minBid}(key, blockNumber);
+
+        IPoolManager.SwapParams memory swapParams = IPoolManager.SwapParams({zeroForOne: true, amountSpecified: 0.01 ether, sqrtPriceLimitX96: 0});
+
+        // Ensure only hook can call checkSwap
+        vm.prank(user1);
+        vm.expectRevert("Only hook!");
+        auction.checkSwap(user1, key, swapParams, "");
+
+        // Simulate the hook making the call and the winner performing the swap
+        vm.roll(blockNumber);
+        vm.prank(hook);
+        auction.checkSwap(user1, key, swapParams, "");
+
+        // Check the bid amount is reset after swap
+        (address bidder, uint256 amount) = auction.winningBids(key.toId(), blockNumber);
+        assertEq(amount, 0);
+    }
+
+}
